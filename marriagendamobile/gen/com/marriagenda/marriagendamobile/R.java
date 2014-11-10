3feb3e38ea338fc0d707ad3a370163e27ee06b81
Robyn <Robyn@10.100.91.95>
2014-11-10 12:57:34 -0500

    Does this work?

--- marriagendamobile/gen/com/marriagenda/marriagendamobile/R.java
+++ marriagendamobile/gen/com/marriagenda/marriagendamobile/R.java
@@ -26,7 +26,7 @@ public final class R {
         public static final int marriagenda_splash_bg=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f080012;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
         public static final int editText1=0x7f080001;
@@ -35,13 +35,22 @@ public final class R {
         public static final int login_button=0x7f080006;
         public static final int login_image=0x7f080004;
         public static final int marriagenda=0x7f080009;
+        public static final int radio0=0x7f08000e;
+        public static final int radio1=0x7f08000f;
+        public static final int radio2=0x7f080011;
+        public static final int radioGroup1=0x7f08000d;
+        public static final int radioGroup2=0x7f080010;
         public static final int register_button=0x7f080007;
+        public static final int textView1=0x7f08000a;
+        public static final int textView2=0x7f08000b;
+        public static final int textView3=0x7f08000c;
         public static final int usernameField=0x7f080005;
     }
     public static final class layout {
         public static final int activity_marriagenda=0x7f030000;
         public static final int login_marriagenda=0x7f030001;
         public static final int marriagenda_splash=0x7f030002;
+        public static final int rsvp=0x7f030003;
     }
     public static final class menu {
         public static final int marriagenda=0x7f070000;
